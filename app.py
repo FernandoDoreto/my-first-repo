fb248fdad52a73c3079ff99a64d185a3e83643df
FernandoDoreto <fernando_doreto@hotmail.com>
2021-03-19 17:01:56 +0000

    Sklearn

--- app.py
+++ app.py
@@ -1,35 +1,26 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+import streamlit as st
+from app_pages.UserStory import PageUserStory
+from app_pages.EDA import PageEDA
+from app_pages.Sklearn import PageSklearn
+from app_pages.Tensorflow import PageTensorflow
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from scr.data_management import LoadIrisDataset
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+def main():
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+    df = LoadIrisDataset()
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+    MenuOptions = [
+        'User Story',
+        'EDA',
+        'Sklearn',
+        'Tensorflow',
+        ]
+    page = st.sidebar.selectbox("Main Menu",MenuOptions,index=0)
+    
+    if page == MenuOptions[0]: PageUserStory(df)
+    elif page == MenuOptions[1]: PageEDA(df)   
+    elif page == MenuOptions[2]: PageSklearn(df,TARGET="Species")
+    elif page == MenuOptions[3]: PageTensorflow(df)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == '__main__': main()
--- app_pages/Sklearn.py
+++ app_pages/Sklearn.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-def PageSklearn(df):
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier 
+
+def PageSklearn(df,TARGET):
     st.write("This is PageSklearn")
+
+    X_train, X_test,y_train, y_test = train_test_split(
+                                        df.drop([TARGET],axis=1),
+                                        df[TARGET],
+                                        test_size=-.2,
+                                        random_state=0,
+                                        stratify=df[TARGET]
+                                        )
+
+    st.write(X_train,y_train)
+
+     
+
+
+
+    # ml_pipeline = Pipeline(
+    # [       
+    #     ("feat_selection",SelectFromModel(DecisionTreeClassifier())),
+    #     ("scaler",StandardScaler()),
+    #     ("model", DecisionTreeClassifier)
+    # ])
