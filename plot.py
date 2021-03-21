a10994356a963ea779b7dd23c6112ad60e012ca7
FernandoDoreto <fernando_doreto@hotmail.com>
2021-03-21 13:45:46 +0000

    Added plot scr

--- plot.py
+++ plot.py
@@ -1,4 +1,9 @@
 from scr.data_management import LoadIrisDataset
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = LoadIrisDataset()
 print(df)
+
+sns.pairplot(df, hue='Species');
+plt.show()
